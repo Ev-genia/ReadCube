036d477dcb92f2476c25e4e6c7744b700144d693
Евгения Костарева <eugenia@mac-book-air-2020.local>
2021-05-26 00:16:16 +0300

    change DragAndDrop

--- Assets/Scripts/dragAndDrop.cs
+++ Assets/Scripts/dragAndDrop.cs
@@ -1,61 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
+﻿using UnityEngine;
+using System.Collections;
 
-public class dragAndDrop : MonoBehaviour
+public class DragAndDrop : MonoBehaviour
 {
-    float ZPosition;
-    Vector3 Offset;
-    bool Dragging;
+    private bool _mouseState;
+    private GameObject target;
+    public Vector3 screenSpace;
+    public Vector3 offset;
 
-    public Camera MainCamera;
-    [Space]
-    [SerializeField]
-    public UnityEvent OnBeginDrag;
-    [SerializeField]
-    public UnityEvent OnEndDrag;
-
-
-
+    // Use this for initialization
     void Start()
     {
-        ZPosition = MainCamera.WorldToScreenPoint(transform.position).z;
+
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Dragging)
+        // Debug.Log(_mouseState);
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, ZPosition);
-            transform.position = MainCamera.ScreenToViewportPoint(position + new Vector3(Offset.x, Offset.y));
+
+            RaycastHit hitInfo;
+            target = GetClickedObject(out hitInfo);
+            if (target != null)
+            {
+                _mouseState = true;
+                screenSpace = Camera.main.WorldToScreenPoint(target.transform.position);
+                offset = target.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _mouseState = false;
+        }
+        if (_mouseState)
+        {
+            //keep track of the mouse position
+            var curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+
+            //convert the screen mouse position to world point and adjust with offset
+            var curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+
+            //update the position of the object in the world
+            target.transform.position = curPosition;
         }
     }
 
 
-    void OnMouseDown()
+    GameObject GetClickedObject(out RaycastHit hit)
     {
-        if (!Dragging)
+        GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
         {
-            BeginDrag();
+            target = hit.collider.gameObject;
         }
-    }
 
-    void OnMouseUp()
-    {
-        EndDrag();
-    }
-
-    public void BeginDrag ()
-    {
-        OnBeginDrag.Invoke();
-        Dragging = true;
-        Offset = MainCamera.WorldToScreenPoint(transform.position) - Input.mousePosition;
-    }
-
-    public void EndDrag ()
-    {
-        OnEndDrag.Invoke();
-        Dragging = false;
+        return target;
     }
 }
