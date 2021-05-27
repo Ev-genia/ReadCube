39f0cd909df67e4c3e9d54de08aa89aa0c545385
Евгения Костарева <eugenia@mac-book-air-2020.local>
2021-05-28 01:58:45 +0300

    change dropdown menu

---
+++ Assets/Scripts/DropDownMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropDownMenu : MonoBehaviour
+{
+    public Text menu;
+
+    public void InputMenu(int value)
+    {
+        if (value == 0)
+        {
+            menu.text = "НУ-НО-НА-НЭ-НЫ-Н";
+        }
+        if (value == 1)
+        {
+            menu.text = "У-О-А-Э-Ы";
+        }
+        if (value == 2)
+        {
+            menu.text = "СУ-СО-СА-СЭ-СЫ-С";
+        }
+    }
+}
