52172fe11ed5573384a1452639a82e29d4400596
Евгения Костарева <eugenia@mac-book-air-2020.local>
2021-05-28 23:07:55 +0300

    add sound

--- Assets/Scripts/DropDownMenu.cs
+++ Assets/Scripts/DropDownMenu.cs
@@ -6,16 +6,24 @@ using UnityEngine.UI;
 public class DropDownMenu : MonoBehaviour
 {
     public Text menu;
+    private AudioSource AudioS;
+
+    void Start()
+    {
+        AudioS = GetComponent<AudioSource>(); 
+    }
 
     public void InputMenu(int value)
     {
         if (value == 0)
         {
             menu.text = "НУ-НО-НА-НЭ-НЫ-Н";
+            
         }
         if (value == 1)
         {
-            menu.text = "У-О-А-Э-Ы";
+            menu.text = "А-О-У-Ы-Э";
+            AudioS.Play();
         }
         if (value == 2)
         {
